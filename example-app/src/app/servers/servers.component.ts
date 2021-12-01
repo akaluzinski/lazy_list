5e1a56bb31f2f666ef79a8b53ce86e3272fafca4
Adrian Kaluzinski <kaluza@gmail.com>
2021-12-01 20:30:06 +0100

    Add minor event

---
+++ example-app/src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  status = 'No servers.';
+
+  onCreateServer(): void {
+    this.status = 'Loading';
+  }
+
+  onUpdateServer(event: any): void {
+    this.status = event.target.value;
+  }
+}
