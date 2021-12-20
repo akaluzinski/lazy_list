6c411f5a555d4b2d7217c68f4cd504f0cdd5ffe1
Adrian Kaluzinski <kaluza@gmail.com>
2021-12-20 20:01:27 +0100

    Add dropdown open toggle directive

---
+++ recipes/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open')
+  isOpen = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('click') toggleOpen(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
