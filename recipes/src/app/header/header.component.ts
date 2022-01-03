1680efded52ade32c20823445621cf830f0d2c9b
Adrian Kaluzinski <kaluza@gmail.com>
2022-01-03 21:24:36 +0100

    Replace event emitter with routerlink

--- recipes/src/app/header/header.component.ts
+++ recipes/src/app/header/header.component.ts
@@ -1,22 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { MenuItem } from '../app.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  @Output() selectedMenu: EventEmitter<any> = new EventEmitter();
-
-  menuItem = MenuItem;
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  selectMenu(menuItem: MenuItem) {
-    console.log('Select: ', menuItem);
-    this.selectedMenu.emit(menuItem);
-  }
-}
+export class HeaderComponent {}
