a603cc0a0f602eb56625e82b6f8cec44e6a553fa
Adrian Kaluzinski <adrian@kaluzinski.eu>
2022-02-16 17:41:40 +0100

    Display error in auth component

--- recipes/src/app/auth/auth.component.ts
+++ recipes/src/app/auth/auth.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { AuthenticationCommand } from './authentication-command.enum';
 import { Router } from '@angular/router';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import { ErrorFrameComponent } from '../shared/error-frame/error-frame.component';
 
 @Component({
   selector: 'app-auth',
@@ -13,10 +15,13 @@ export class AuthComponent {
 
   isLogin = true;
   isLoading = false;
-  errorMessage = '';
+
+  @ViewChild(PlaceholderDirective, { static: false })
+  errorPlaceholder: PlaceholderDirective;
 
   constructor(private readonly authService: AuthService,
-              private readonly router: Router) {
+              private readonly router: Router,
+              private readonly componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   toggleMode(): void {
@@ -42,12 +47,16 @@ export class AuthComponent {
     this.authService.emailAuthentication(email, password, command).subscribe(user => {
       console.log(command, ' successful', email);
       this.isLoading = false;
-      this.errorMessage = '';
       this.router.navigate(['/recipes']);
     }, error => {
       this.isLoading = false;
-      console.error(command, ' failed', error);
-      this.errorMessage = error;
+      this.showError(error);
     });
+  }
+
+  private showError(error: string): void {
+    const errorFrameFactory = this.componentFactoryResolver.resolveComponentFactory(ErrorFrameComponent);
+    const errorFrameInstance = this.errorPlaceholder.viewContainerRef.createComponent(errorFrameFactory);
+    errorFrameInstance.instance.errorMessage = error;
   }
 }
--- recipes/src/app/shared/error-frame/error-frame.component.ts
+++ recipes/src/app/shared/error-frame/error-frame.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-error-frame',
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class ErrorFrameComponent {
 
+  @Input()
   errorMessage = '';
 
 }
