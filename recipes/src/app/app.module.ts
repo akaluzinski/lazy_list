1f7d16cf6806c5fc00d275ac94e2a4d34fc8287c
Adrian Kaluzinski <adrian@kaluzinski.eu>
2022-02-19 10:25:47 +0100

    Extract shopping list modules

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -17,20 +15,27 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { ErrorFrameComponent } from './shared/error-frame/error-frame.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipe.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
     ErrorFrameComponent,
     PlaceholderDirective
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule, RecipesModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RecipesModule,
+    ShoppingListModule
+  ],
   providers: [ShoppingListService, RecipeService, {
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptorService,
