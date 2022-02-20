9b640e75daacc554b954416ad409509e7f819125
Adrian Kaluzinski <adrian@kaluzinski.eu>
2022-02-20 19:32:56 +0100

    Change module preloading strategy

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +8,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
+
+const routesConfig = { preloadingStrategy: PreloadAllModules };
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -26,7 +28,7 @@ const routes: Routes = [
     HttpClientModule,
     CoreModule,
     AuthModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, routesConfig)
   ],
   bootstrap: [AppComponent],
 })
