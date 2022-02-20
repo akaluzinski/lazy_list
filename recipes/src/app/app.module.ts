7cc848dd3bf2285a4f87a5251a5b6e042764b56c
Adrian Kaluzinski <adrian@kaluzinski.eu>
2022-02-20 19:28:40 +0100

    Add lazy loading to ShoppingListModule

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -5,17 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
 
-
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: () => import('./recipes/recipe.module').then(m => m.RecipesModule) }
+  { path: 'recipes', loadChildren: () => import('./recipes/recipe.module').then(m => m.RecipesModule) },
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) }
 ];
-
 
 @NgModule({
   declarations: [
@@ -26,7 +24,6 @@ const routes: Routes = [
     BrowserModule,
     SharedModule,
     HttpClientModule,
-    ShoppingListModule,
     CoreModule,
     AuthModule,
     RouterModule.forRoot(routes)
